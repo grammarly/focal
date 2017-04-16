3216cf1ae33f19a60562b157c938ca9e0ffa1fa6
Sergey Yavnyi <blacktaxi@gmail.com>
2017-04-16 07:01:30 -0700

    Simplify app loader

--- examples/all/src/index.tsx
+++ examples/all/src/index.tsx
@@ -1,22 +1,36 @@
-import { App } from './app'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Atom } from '@grammarly/focal'
+import * as App from './app'
 
 // hot reload support
-declare const require: (name: String) => any
+declare const require: (path: string) => any
 declare const module: {
   hot?: { accept: (path?: string, callback?: () => void) => void }
 }
 
-const targetEl = document.getElementById('app')
-let app = new App(targetEl!)
+const appState = Atom.create(App.defaultState)
+
+appState.subscribe(s => {
+  console.log('App state changed: ', s)
+})
+
+// inject app state in global for debugging
+; (window as any).appState = appState
+
+function startApp(C: typeof App.AppComponent) {
+  const targetEl = document.getElementById('app')
+  if (targetEl == null)
+    throw new Error('React app target element not found. Wrong HTML file?')
+
+  ReactDOM.render(<C state={appState} />, targetEl)
+}
 
 if (module.hot) {
   module.hot.accept('./app', () => {
-    const newAppModule = require('./app')
-
-    const newApp = (new newAppModule.App(targetEl , app.state))
-    app = newApp
-    app.start()
+    const newAppModule = require('./app') as typeof App
+    startApp(newAppModule.AppComponent)
   })
 }
 
-app.start()
+startApp(App.AppComponent)

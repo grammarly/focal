f651d2b7a93f7ea2d3ffc6ef1cb4e5384533a2ed
Sergey Yavnyi <blacktaxi@gmail.com>
2017-10-06 09:10:30 -0700

    Move F namespace to react/index

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,11 @@ export { Lens, Prism, Optic } from './lens/index'
 export { Atom, ReadOnlyAtom } from './atom/index'
 export { Option } from './utils'
 
-// @NOTE need the following import to prevent TS error
-// "variable is using name from external module but can not be named"
-// tslint:disable-next-line no-unused-variable
-import { LiftedIntrinsics } from './react/intrinsic'
-import { createLiftedIntrinsics } from './react/intrinsic'
-
-export const F = createLiftedIntrinsics()
-
 export {
   bind,
   lift,
   reactiveList,
   classes,
-  bindElementProps
-} from './react/index'
+  bindElementProps,
+  F
+} from './react'
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,1 +1,9 @@
 export * from './react'
+
+// @NOTE need the following import to prevent TS error
+// "variable is using name from external module but can not be named"
+// tslint:disable-next-line no-unused-variable
+import { LiftedIntrinsics } from './intrinsic'
+import { createLiftedIntrinsics } from './intrinsic'
+
+export const F = createLiftedIntrinsics()

49dca433872442418d7941e2694bff22240a3426
Sergey Yavnyi <blacktaxi@gmail.com>
2018-02-12 15:39:55 -0800

    Implement source sub refcount for AtomViewImpl

--- src/atom/base.ts
+++ src/atom/base.ts
@@ -417,7 +417,7 @@ class AtomViewImpl<TSource, TDest> extends AbstractReadOnlyAtom<TDest> {
       : this._getter(this._source.get())
   }
 
-  private _onSourceValue(x: TSource) {
+  private _onSourceValue = (x: TSource) => {
     const prevValue = this.getValue()
     const next = this._getter(x)
 
@@ -425,22 +425,34 @@ class AtomViewImpl<TSource, TDest> extends AbstractReadOnlyAtom<TDest> {
       this.next(next)
   }
 
-  private __onSourceValue: ((x: TSource) => void) | undefined = undefined
   private _subscription: Subscription | null = null
+  private _refCount = 0
 
   // Rx method overrides
   _subscribe(subscriber: Subscriber<TDest>) { // tslint:disable-line function-name
     if (!this._subscription) {
-      this.__onSourceValue = (value: TSource) => this._onSourceValue(value)
-      this._subscription = this._source.subscribe(this.__onSourceValue)
+      this._subscription = this._source.subscribe(this._onSourceValue)
     }
+    this._refCount++
 
-    return super._subscribe(subscriber)
+    const sub = new Subscription(() => {
+      if (--this._refCount <= 0 && this._subscription) {
+        this._subscription.unsubscribe()
+        this._subscription = null
+      }
+    })
+    sub.add(super._subscribe(subscriber))
+
+    return sub
   }
 
   unsubscribe() {
-    if (this._subscription) this._subscription.unsubscribe()
-    this.__onSourceValue = undefined
+    if (this._subscription) {
+      this._subscription.unsubscribe()
+      this._subscription = null
+    }
+    this._refCount = 0
+
     super.unsubscribe()
   }
 }

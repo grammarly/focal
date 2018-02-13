d88999ce098932e16e91baf9c029a4e426bfa3b9
Sergey Yavnyi <blacktaxi@gmail.com>
2018-02-12 16:53:24 -0800

    Apply the same fix to lens

--- src/atom/base.ts
+++ src/atom/base.ts
@@ -368,22 +368,34 @@ class LensedAtom<TSource, TDest> extends AbstractAtom<TDest> {
       this.next(next)
   }
 
-  private __onSourceValue: ((x: TSource) => void) | undefined = undefined
   private _subscription: Subscription | null = null
+  private _refCount = 0
 
   // Rx method overrides
   _subscribe(subscriber: Subscriber<TDest>) { // tslint:disable-line function-name
     if (!this._subscription) {
-      this.__onSourceValue = (value: TSource) => this._onSourceValue(value)
-      this._subscription = this._source.subscribe(this.__onSourceValue)
+      this._subscription = this._source.subscribe(x => this._onSourceValue(x))
     }
+    this._refCount++
 
-    return super._subscribe(subscriber)
+    const sub = new Subscription(() => {
+      if (--this._refCount <= 0 && this._subscription) {
+        this._subscription.unsubscribe()
+        this._subscription = null
+      }
+    })
+    sub.add(super._subscribe(subscriber))
+
+    return sub
   }
 
   unsubscribe() {
-    if (this._subscription) this._subscription.unsubscribe()
-    this.__onSourceValue = undefined
+    if (this._subscription) {
+      this._subscription.unsubscribe()
+      this._subscription = null
+    }
+    this._refCount = 0
+
     super.unsubscribe()
   }
 }

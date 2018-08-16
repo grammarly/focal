05693ba75c2c7e9e4f828f85b692043bfafe33eb
Sergey Yavnyi <blacktaxi@gmail.com>
2018-08-16 16:14:30 -0700

    Use TS 2.8 in observablePropTypes

--- src/react/observablePropTypes.ts
+++ src/react/observablePropTypes.ts
@@ -2,55 +2,26 @@
  * This module defines augmented React element/props types that accept
  * observable values.
  *
- * @NOTE:
- * Q: Why doesn't this just use mapped types?
- * A: Mostly because right now it is not possible have an interface that extends
- * a mapped type. Because of that we will have to have generic structural types,
- * which get inlined into intrinsic component function definitions, expanding the
- * intrinsic.d.ts to a 3MB and slowing down VSCode to the point of uselessness.
- *
- * Right now we are using a mapped type of ObservableCSSReactProperties, and it works out fine
- * because in the end it's a property of the ObservableReactHTMLProps interface, and is not
- * inlined in intrinsic component function definitions.
- *
- * Hopefully we can find a way to use mapped types for all of these types in future.
- *
  * @module
  */
 import * as React from 'react'
-import { Observable } from 'rxjs/Observable'
+import { ObservableLike } from './react'
 
-export type ObservableOr<T> = Observable<T> | T
-export type AcceptObservableValues<T> = { [K in keyof T]: ObservableOr<T[K]> }
+export type ObservableLikeRecord<T> = { [K in keyof T]: ObservableLike<T[K]> }
 
-/**
- * It's a workaround to set proper type for style
- */
-export interface ObservableCSSReactProperties {
-  style?: ObservableOr<React.CSSProperties> | AcceptObservableValues<React.CSSProperties>
-}
+export type ObservableReactCSSProperties =
+  ObservableLike<React.CSSProperties> | ObservableLikeRecord<React.CSSProperties>
 
 export interface ObservableReactChildren {
-  children?: ObservableOr<React.ReactNode>
+  children?: ObservableLike<React.ReactNode>
 }
 
-export interface ObservableReactHTMLAttributes<E>
-  extends AcceptObservableValues<React.AllHTMLAttributes<E>> {}
+export type ObservableReactHTMLAttributes<
+  E, A extends React.HTMLAttributes<E> = React.AllHTMLAttributes<E>
+> = ObservableLikeRecord<Pick<A, Exclude<keyof A, 'style'>>> & {
+  style?: ObservableReactCSSProperties
+}
 
-/**
- * It's a workaround to set proper type for style
- *
- * TS 2.8 provides more eloquent way to do it
- *
- * type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
- * export interface ObservableReactHTMLProps<E>
- *  extends AcceptObservableValues<Omit<React.AllHTMLAttributes<E>>, 'style'> {
- *   style?: ObservableOr<React.CSSProperties> | AcceptObservableValues<React.CSSProperties>
- * }
- *
- */
-export type ObservableReactDOMAttributes<E> = ObservableReactHTMLAttributes<E> &
-  ObservableCSSReactProperties
 export interface ObservableReactHTMLProps<E>
-  extends ObservableReactDOMAttributes<E>,
+  extends ObservableReactHTMLAttributes<E>,
     React.ClassAttributes<E> {}
--- src/react/react.ts
+++ src/react/react.ts
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { structEq } from './../utils'
 import { Atom } from './../atom'
-import { ObservableReactHTMLProps } from './observablePropTypes'
 import { warning, getReactComponentName, DEV_ENV } from './../utils'
 import { Observable, ObservableInput } from 'rxjs/Observable'
 import { Subscription as RxSubscription } from 'rxjs/Subscription'

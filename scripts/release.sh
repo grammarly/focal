7c613e5e94be7c20fe2ec3f7921498fd69369c0e
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2020-06-16 09:20:07 +0300

    Move contributors to AUTHORS file
    Example: https://github.com/facebook/react/blob/master/AUTHORS

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,6 +13,8 @@ echo Publishing v${VER}...
 
 cp ./README.md ./packages/focal/README.md && cp ./README.md ./packages/focal-atom/README.md
 cp ./LICENSE ./packages/focal/LICENSE && cp ./LICENSE ./packages/focal-atom/LICENSE
+# Copy AUTHORS, so it will be used as value for "contributors" in package.json
+cp ./LICENSE ./packages/focal/AUTHORS && cp ./LICENSE ./packages/focal-atom/AUTHORS
 
 cd $BASEDIR/packages/focal-atom && npm publish https://github.com/grammarly/focal/releases/download/v$VER/grammarly-focal-atom-v$VER.tgz --access public --dry-run
 cd $BASEDIR/packages/focal && npm publish https://github.com/grammarly/focal/releases/download/v$VER/grammarly-focal-v$VER.tgz --access public --dry-run

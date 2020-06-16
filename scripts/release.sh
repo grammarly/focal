329877f21430c9bb84b9b213677ae1575b6e7b6d
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2020-06-16 15:02:33 +0300

    Better release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,10 +16,16 @@ cp ./LICENSE ./packages/focal/LICENSE && cp ./LICENSE ./packages/focal-atom/LICE
 # Copy AUTHORS, so it will be used as value for "contributors" in package.json
 cp ./LICENSE ./packages/focal/AUTHORS && cp ./LICENSE ./packages/focal-atom/AUTHORS
 
+echo "Publishing @grammarly/focal-atom with '$VER'"
 cd $BASEDIR/packages/focal-atom && npm publish https://github.com/grammarly/focal/releases/download/v$VER/grammarly-focal-atom-v$VER.tgz --access public --dry-run
+echo "\nACTION REQUIRED!!!\n"
+echo "Manually update @grammarly/focal to use '@grammarly/focal-atom@$VER' and press any key:"
+read $ANYKEY
+echo "Publishing @grammarly/focal with '$VER'"
 cd $BASEDIR/packages/focal && npm publish https://github.com/grammarly/focal/releases/download/v$VER/grammarly-focal-v$VER.tgz --access public --dry-run
 
 rm -rf $BASEDIR/packages/focal/README.md && rm -rf $BASEDIR/packages/focal-atom/README.md
 rm -rf $BASEDIR/packages/focal/LICENSE && rm -rf $BASEDIR/packages/focal-atom/LICENSE
+rm -rf $BASEDIR/packages/focal/AUTHORS && rm -rf $BASEDIR/packages/focal-atom/AUTHORS
 
 git push && git push --tags

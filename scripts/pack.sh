23a373f07d1a7ae065ddf1e342b93adbb5386c3d
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2021-04-04 12:09:50 +0300

    use prepack and postpack to copy License and Readme

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,15 +2,12 @@
 
 echo Packaging...
 
-cp ./README.md ./packages/focal/README.md && cp ./README.md ./packages/focal-atom/README.md
-cp ./LICENSE ./packages/focal/LICENSE && cp ./LICENSE ./packages/focal-atom/LICENSE
 # Copy AUTHORS, so it will be used as value for "contributors" in package.json
+# TODO: That doesn't work, find a better way to setup contributors
 cp ./AUTHORS ./packages/focal/AUTHORS && cp ./AUTHORS ./packages/focal-atom/AUTHORS
 
 cd ./packages/focal && npm pack && cd ../..
 cd ./packages/focal-atom && npm pack && cd ../..
 # yarn workspace @grammarly/focal-atom pack
 
-rm -rf ./packages/focal/README.md && rm -rf ./packages/focal-atom/README.md
-rm -rf ./packages/focal/LICENSE && rm -rf ./packages/focal-atom/LICENSE
 rm -rf ./packages/focal/AUTHORS && rm -rf ./packages/focal-atom/AUTHORS

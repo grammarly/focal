6c7998daed872d1d8273a559b944cc8189d47bde
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2021-04-06 20:04:11 +0300

    update release scripts

--- .github/workflows/Readme.md
+++ .github/workflows/Readme.md
@@ -4,7 +4,7 @@
 
 1. Create a pull request for release "Release vX.X.X"
 2. Run in root `yarn bump-release X.X.X`
-3. run `yarn bump-release` to bump focal version, update dependancies in private packages and create a commit
+3. run `yarn bump-release` to bump focal and focal-atom version, update dependancies in private packages and create a commit
 4. Merge and manually create [GitHub Release](https://github.com/grammarly/focal/releases)
 5. On release, "publish" GitHub Actions will trigger build and attach tarball to "Release". For details, see `release.yml`
 
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VER=$1
+
 echo Bumping version to v${VER}...
 
 cd packages/focal && yarn version --new-version $VER --no-git-tag-version && cd ../..
@@ -10,12 +12,13 @@ sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/te
 echo Bumping version to v${VER}...
 
 cd packages/focal-atom && yarn version --new-version $VER --no-git-tag-version && cd ../..
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/examples/all/package.json
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/examples/todomvc/package.json
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/test/package.json
+sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/focal/package.json
+sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/examples/all/package.json
+sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/examples/todomvc/package.json
+sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/test/package.json
 
 # update yarn.lock
-yarn
+yarn && yarn test
 
 # commit changes
 git add .

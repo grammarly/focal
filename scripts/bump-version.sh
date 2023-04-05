3ba2f9f19449bdbd5fdabbd2658b713ba137b8e4
Oleksii Levzhynskyi <oleksii.levzhynskyi@grammarly.com>
2023-04-05 15:32:46 +0300

    Update bump-version script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+# exit when any command fails
+set -e
+
 VER=$1
 
 echo Bumping version to v${VER}...
 
 cd packages/focal && yarn version --new-version $VER --no-git-tag-version && cd ../..
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/examples/all/package.json
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/examples/todomvc/package.json
-sed -i '' 's/grammarly\/focal":.*$/grammarly\/focal": "'${VER}'",/g' packages/test/package.json
+sed -i '' 's/grammarly\/focal":.*"\(.*\)$/grammarly\/focal": "'${VER}'"\1/g' packages/examples/all/package.json
+sed -i '' 's/grammarly\/focal":.*"\(.*\)$/grammarly\/focal": "'${VER}'"\1/g' packages/examples/todomvc/package.json
+sed -i '' 's/grammarly\/focal":.*"\(.*\)$/grammarly\/focal": "'${VER}'"\1/g' packages/test/package.json
 
 echo Bumping version to v${VER}...
 
 cd packages/focal-atom && yarn version --new-version $VER --no-git-tag-version && cd ../..
-sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/focal/package.json
-sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/examples/all/package.json
-sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/examples/todomvc/package.json
-sed -i '' 's/grammarly\/focal-atom":.*$/grammarly\/focal-atom": "'${VER}'",/g' packages/test/package.json
+sed -i '' 's/grammarly\/focal-atom":.*"\(.*\)$/grammarly\/focal-atom": "^'${VER}'"\1/g' packages/focal/package.json
+sed -i '' 's/grammarly\/focal-atom":.*"\(.*\)$/grammarly\/focal-atom": "'${VER}'"\1/g' packages/examples/all/package.json
+sed -i '' 's/grammarly\/focal-atom":.*"\(.*\)$/grammarly\/focal-atom": "'${VER}'"\1/g' packages/examples/todomvc/package.json
+sed -i '' 's/grammarly\/focal-atom":.*"\(.*\)$/grammarly\/focal-atom": "'${VER}'"\1/g' packages/test/package.json
 
 # update yarn.lock
-yarn && yarn test
+yarn && yarn build && yarn test
 
 # commit changes
 git add .

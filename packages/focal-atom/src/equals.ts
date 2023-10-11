83a662ba2a0c9165adbad884e5ed7370b9a97afa
Igor Oleinikov <igor@oleinikov.ru>
2023-10-11 16:33:18 -0700

    Simplify check for promises in equals

--- packages/focal-atom/src/equals.ts
+++ packages/focal-atom/src/equals.ts
@@ -35,10 +35,9 @@ function arrayFromIterator<T>(iter: Iterator<T>) {
   return result
 }
 
-function functionName(f: Function) {
-  // String(x => x) evaluates to "x => x", so the pattern may not match.
-  const match = String(f).match(/^function (\w*)/)
-  return match == null ? '' : match[1]
+function isPromise(a: unknown) {
+  // The function checks that is a 'thenable'
+  return Boolean(value && typeof (value as Promise<unknown>).then === 'function')
 }
 
 function has(prop: string, obj: any) {
@@ -207,8 +206,7 @@ export function structEq(a: any, b: any, stackA: any[] = [], stackB: any[] = [])
     case 'Arguments':
     case 'Array':
     case 'Object':
-      if (typeof a.constructor === 'function' &&
-        functionName(a.constructor) === 'Promise') {
+      if (isPromise(a)) {
         return a === b
       }
       break
